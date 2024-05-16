f03d0fd6ae0ccddf0a71fac95c38d18d3288d02a
irchamali <id.irchamali@gmail.com>
2024-05-16 14:29:37 +0700

    Add some file for basic php

---
+++ operator.php
@@ -0,0 +1,12 @@
+<?php
+    $x = 10;
+    $y = 3;
+
+    echo -$x . '<hr>'; 
+    echo $x+$y . '<hr>';
+    echo $x-$y . '<hr>';
+    echo $x*$y . '<hr>';
+    echo $x/$y . '<hr>';
+    echo $x%$y . '<hr>';
+
+?>
---
+++ tipedata.php
@@ -0,0 +1,7 @@
+<?php
+    $angka1 = 1;
+    $angka2 = 2;
+
+    echo $angka1 . $angka2;
+    echo '<br>';
+    echo $angka1 + $angka2;
